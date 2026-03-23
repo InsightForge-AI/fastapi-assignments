667aa02179e7546a9dbfd065ec62c2afefdcaff0
prasanth <adityaprasanth519@gmail.com>
2026-03-23 18:32:27 +0530

    day1 : api is created

--- Prasanth/main.py
+++ Prasanth/main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-# Create the app
 app = FastAPI()
 
-# Root endpoint: "/"
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to my first FastAPI app!"}
+def root():
+    return {"message": "Welcome to FastAPI!"}
 
-# Hello endpoint: "/hello"
 @app.get("/hello")
-def say_hello():
-    return {"message": "Hello, beginner! You just built your first API "}
+def hello():
+    return {"message": "Hello, world!"}
