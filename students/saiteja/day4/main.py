a2307b3ba473f7926688d692c99b15123033bc28
tejachennu41 <tejachennu2423@gmail.com>
2026-03-27 18:46:55 +0530

    final fastapi project

---
+++ students/saiteja/day4/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Request body model
+class User(BaseModel):
+    name: str
+    age: int
+
+# GET API
+@app.get("/")
+def home():
+    return {"message": "Day 4 API Running"}
+
+# POST API
+@app.post("/user")
+def create_user(user: User):
+    return {
+        "message": f"Hello {user.name}, you are {user.age} years old"
+    }
