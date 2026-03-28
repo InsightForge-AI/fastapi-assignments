3aca2659927b6b207c451ef6b4a9b66fd43cb626
SriHarshaMoravaneni <48232389+SriHarshaMoravaneni@users.noreply.github.com>
2026-03-28 15:26:43 +0530

    Corrected post
    Finally corrected post and understood difference between get and post

--- SriHarsha/Day3.py
+++ SriHarsha/Day3.py
@@ -3,21 +3,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Request model
 class Person(BaseModel):
     name: str
     age: int
 
-# Root API
-@app.get("/")
-def read_root():
-    return {"message": "API to get name and age"}
-
 # POST API
-@app.post("/person/")
-async def create_person(person: Person) -> Person:
-    return person
-
-# GET API
-@app.get("/person")
-async def read_item(name: str, age: int) -> Person:
-    return Person(name=name, age=age)
+@app.post("/person")
+async def create_person(person: Person):
+    return {
+        "message": f"Hello {person.name}, age {person.age} received!"
+    }
