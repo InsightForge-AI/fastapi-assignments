5004f59695a010a820a677b6c6c3854e3a7e642c
Ujjwal Vats <vatsujjwal2002@gmail.com>
2026-03-29 18:49:12 +0530

    final day 3 changes

--- ujjwal/Assignments/main3.py
+++ ujjwal/Assignments/main3.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.params import Body
 app = FastAPI()
 
+@app.get("/")
+async def first():
+    return {"message":"Hi, you can create post here"}
+
 @app.post("/createpost")
 def create_post(payload:dict=Body(...)):
     print(payload)
